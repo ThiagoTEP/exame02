d0119b8d2b5c0418e49886fb3f41b352ddff3b14
Thiago Evaristo De Paula <thiagodepaulapt@gmail.com>
2025-07-29 10:29:50 +0100

    str capitalizer

---
+++ ranking03/str_capitalizer.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+
+		if ((str[i -1] == ' ' || i == 0) &&(str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+
+int main(int ac, char *av[])
+{
+	if (ac > 1)
+	{
+		int i = 1;
+		while (i < ac)
+		{
+			ft_putstr(av[i]);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return 0;
+}
